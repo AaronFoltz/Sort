a78f5dc2b38624aca7108e4123efbd9eeef9513b
Aaron Foltz <Onemean98GT@gmail.com>
2011-08-26 10:05:32 -0400

    Added SelectionSort

--- Sort.py
+++ Sort.py
@@ -7,7 +7,6 @@ def BubbleSort(list):
 	# While swapped, keep looping
 	while(swapped):
 		swapped = False
-		
 		# Loop through each element in the list
 		for i in range(0,n-1):
 			
@@ -22,3 +21,23 @@ def BubbleSort(list):
 	print list
 
 BubbleSort([5,1,4,2,8,5,1,4,2,8])
+
+
+def SelectionSort(list):
+	# Iterate through each element in the array
+	for i in range(0, len(list)):
+		# The minimum is at this index so far
+		min = i;
+		for j in range(i+1, len(list)):
+			if(list[j] < list[min]): 
+				min = j
+		
+		# Swap the values
+		temp = list[i]
+		list[i] = list[min]
+		list[min] = temp
+				
+	
+	print list	
+	
+SelectionSort([5,1,4,2,8,5,1,4,2,8])
